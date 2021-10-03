a585a39817964d92b7251cf6fee1503eae136f80
Khaiz Badaru Tammam <tammam.dev@gmail.com>
2021-10-03 14:22:27 +0700

    [Added]Post CRUD

--- repository/post_repository.go
+++ repository/post_repository.go
@@ -9,5 +9,9 @@ type (
 	PostRepository interface {
 		Insert(ctx context.Context, post entity.Post) (entity.Post, error)
 		FindById(ctx context.Context, id int32) (entity.Post, error)
+		FindAll(ctx context.Context) ([]entity.Post, error)
+		UpdatePost(ctx context.Context, post entity.Post, id int32) (entity.Post, error)
+		FindPostByUser(ctx context.Context, userId int32) ([]entity.Post, error)
+		DeletePost(ctx context.Context, id int32) error
 	}
 )
--- repository/post_repository_impl.go
+++ repository/post_repository_impl.go
@@ -56,3 +56,75 @@ func (repository *postRepositoryImpl) FindById(ctx context.Context, id int32) (e
 		return post, errors.New("Id " + strconv.Itoa(int(id)) + "Tidak Ditemukan")
 	}
 }
+
+func (repository *postRepositoryImpl) FindAll(ctx context.Context) ([]entity.Post, error) {
+	query := "SELECT posts.id, posts.title, posts.body, posts.created_at, users.name FROM posts INNER JOIN users ON users.id = posts.created_by"
+
+	result, err := repository.DB.QueryContext(ctx, query)
+	user := entity.User{}
+	posts := []entity.Post{}
+	if err != nil {
+		return posts, err
+	}
+
+	defer result.Close()
+
+	for result.Next() {
+		post := entity.Post{}
+		post.UserName = user.Name
+		result.Scan(&post.Id, &post.Title, &post.Body, &post.CreatedAt, &post.UserName)
+
+		posts = append(posts, post)
+	}
+
+	return posts, nil
+}
+
+func (repository *postRepositoryImpl) UpdatePost(ctx context.Context, post entity.Post, id int32) (entity.Post, error) {
+	query := "UPDATE posts SET title = ?, body = ? WHERE id = ?"
+
+	result, err := repository.DB.QueryContext(ctx, query, post.Title, post.Body, id)
+
+	defer result.Close()
+	if err != nil {
+		return post, err
+	}
+
+	return post, nil
+}
+
+func (repository *postRepositoryImpl) FindPostByUser(ctx context.Context, userId int32) ([]entity.Post, error) {
+	query := "SELECT posts.id, posts.title, posts.body, posts.created_at, users.name FROM posts INNER JOIN users ON users.id = posts.created_by WHERE created_by = ?"
+
+	result, err := repository.DB.QueryContext(ctx, query, userId)
+
+	defer result.Close()
+	posts := []entity.Post{}
+	user := entity.User{}
+	if err != nil {
+		return posts, err
+	}
+
+	for result.Next() {
+		post := entity.Post{}
+		post.UserName = user.Name
+		result.Scan(&post.Id, &post.Title, &post.Body, &post.CreatedAt, &post.UserName)
+
+		posts = append(posts, post)
+	}
+
+	return posts, err
+}
+
+func (repository *postRepositoryImpl) DeletePost(ctx context.Context, id int32) error {
+	query := "DELETE FROM posts WHERE id = ?"
+	deleteData, err := repository.DB.QueryContext(ctx, query, id)
+
+	if err != nil {
+		return err
+	}
+
+	defer deleteData.Close()
+
+	return nil
+}
